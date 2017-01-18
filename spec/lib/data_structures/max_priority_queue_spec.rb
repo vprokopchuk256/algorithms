2c4ff306d0718ecc5101b64928d4d28d0da75a54
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2017-01-17 19:23:11 -0600

    max priority queue

---
+++ spec/lib/data_structures/max_priority_queue_spec.rb
@@ -0,0 +1,45 @@
+require_relative '../../../lib/data_structures/max_priority_queue.rb'
+
+RSpec.describe Algorithms::DataStructures::MaxPriorityQueue do
+  subject(:queue) { described_class.new }
+
+  it { is_expected.to be_empty }
+
+  it 'does not have max element yet' do
+    expect(queue.del_max).to eq(nil)
+  end
+
+  context 'when 1 element is added' do
+    before { queue.insert(2) }
+
+    it { is_expected.not_to be_empty }
+
+    describe 'del_max' do
+      subject { queue.del_max }
+
+      it 'equals to that element' do
+        expect(subject).to eq(2)
+      end
+
+      it 'returns queue to the empty state' do
+        expect{ subject }.to change{ queue.empty? }.to(true)
+      end
+    end
+  end
+
+  context 'when 3 elements were added' do
+    before do
+      queue.insert(3)
+      queue.insert(1)
+      queue.insert(2)
+    end
+
+    describe 'del_max' do
+      it 'equals to that element' do
+        expect(queue.del_max).to eq(3)
+        expect(queue.del_max).to eq(2)
+        expect(queue.del_max).to eq(1)
+      end
+    end
+  end
+end

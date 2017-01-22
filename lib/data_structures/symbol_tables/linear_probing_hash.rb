9ea44b4b0d8ca0ee1d0cf8643f37cb821ab0f235
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2017-01-22 00:30:01 -0600

    linear probing hash table

---
+++ lib/data_structures/symbol_tables/linear_probing_hash.rb
@@ -0,0 +1,48 @@
+module Algorithms
+  module DataStructures
+    module SymbolTables
+      class LinearProbingHash
+        M = 300001
+        private_constant :M
+
+        def put(key, value)
+          i = hash(key)
+
+          while keys[i]
+            break if keys[i] == key
+            i = (i + 1) % M
+          end
+
+          keys[i] = key
+          values[i] = value
+        end
+
+        def get(key)
+          i = hash(key)
+
+          while keys[i]
+            return values[i] if keys[i] == key
+
+            i = (i + 1) % M
+          end
+
+          nil
+        end
+
+        private
+
+        def values
+          @values ||= Array.new(M)
+        end
+
+        def keys
+          @keys ||= Array.new(M)
+        end
+
+        def hash(key)
+          (key.hash && 0x7fffffff) % M
+        end
+      end
+    end
+  end
+end

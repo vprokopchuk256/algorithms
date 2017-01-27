bd09b7aab0580d7eec6ee0cb84cca8c858974abf
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2017-01-26 21:32:56 -0600

    deep first graph enumeration

---
+++ lib/algorithms/graph/deep_first_paths.rb
@@ -0,0 +1,53 @@
+module Algorithms
+  module Graph
+    class DeepFirstPaths
+      include Enumerable
+
+      class EnumerationBlock
+        attr_reader :graph, :block
+
+        def initialize graph, &block
+          @graph = graph
+          @block = block
+        end
+
+        def visit(v)
+          return if visited?(v)
+
+          mark_as_visited(v)
+          block.call(v)
+
+          graph.adj(v).each(&method(:visit))
+        end
+
+        private
+
+        def visited_vertices
+          @visited_vertices ||= Array.new(graph.vertices_number, false)
+        end
+
+        def visited?(v)
+          visited_vertices[v]
+        end
+
+        def mark_as_visited(v)
+          visited_vertices[v] = true
+        end
+      end
+      private_constant :EnumerationBlock
+
+      attr_reader :graph, :from
+
+      def initialize(graph, from)
+        @graph = graph
+        @from = from
+      end
+
+      def each(&block)
+        return enum_for(:each) unless block_given?
+
+        EnumerationBlock.new(graph, &block).visit(from)
+      end
+    end
+  end
+end
---
+++ spec/lib/algorithms/graph/deep_first_paths_spec.rb
@@ -0,0 +1,45 @@
+require_relative '../../../../lib/algorithms/graph/deep_first_paths'
+require_relative '../../../../lib/data_structures/graphs/indirected'
+
+RSpec.describe Algorithms::Graph::DeepFirstPaths do
+  let(:number_of_vertices) { 4 }
+  let(:graph) { DataStructures::Graphs::Indirected.new(number_of_vertices) }
+
+  subject(:paths) { described_class.new(graph, 2) }
+
+  it { is_expected.to match_array([2]) }
+
+  context 'with one edge' do
+    before { graph.add_edge(2, 1) }
+
+    it { is_expected.to match_array([1, 2]) }
+  end
+
+  context 'with two connected neighbors' do
+    before do
+      graph.add_edge(2, 1)
+      graph.add_edge(2, 3)
+    end
+
+    it { is_expected.to match_array([1, 2, 3]) }
+  end
+
+  context 'with two sequentially connected neighbors' do
+    before do
+      graph.add_edge(2, 3)
+      graph.add_edge(3, 4)
+    end
+
+    it { is_expected.to match_array([2, 3, 4]) }
+  end
+
+  context 'with cycle' do
+    before do
+      graph.add_edge(2, 3)
+      graph.add_edge(3, 4)
+      graph.add_edge(4, 2)
+    end
+
+    it { is_expected.to match_array([2, 3, 4]) }
+  end
+end

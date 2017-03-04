94f823153a8805e705e15e3666c3540793ce571c
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2017-03-04 12:08:31 -0600

    sp

--- lib/data_structures/graphs/sp.rb
+++ lib/data_structures/graphs/sp.rb
@@ -1,7 +1,52 @@
+require_relative 'directed_edge'
+
 module Algorithms
   module DataStructures
     module Graphs
       class SP
+        Error = Class.new(StandardError)
+
+        attr_reader :s
+
+        def initialize(s)
+          @s = s
+
+          @distance_to = []
+          @distance_to[s] = 0
+
+          @edge_to = []
+        end
+
+        def distance_to(v)
+          raise Error unless @distance_to[v]
+
+          return @distance_to[v]
+        end
+
+        def path_to(v)
+          return [s] if v == s
+          raise Error unless edge = edge_to(v)
+
+          res = [v]
+
+          while edge
+            res.unshift(edge.from)
+            edge = edge_to(edge.from)
+          end
+
+          res
+        end
+
+        def relax(edge)
+          @edge_to[edge.to] = edge
+          @distance_to[edge.to] = edge.weight + (distance_to(edge.from) || 0)
+        end
+
+        private
+
+        def edge_to(v)
+          @edge_to[v]
+        end
       end
     end
   end
--- spec/lib/data_structures/graphs/sp_spec.rb
+++ spec/lib/data_structures/graphs/sp_spec.rb
@@ -1,4 +1,88 @@
-require_relative '../../../../lib/data_structures/graphs/sp.rb'
+require_relative '../../../../lib/data_structures/graphs/sp'
+require_relative '../../../../lib/data_structures/graphs/directed_edge'
 
 RSpec.describe Algorithms::DataStructures::Graphs::SP do
+  let(:s) { 0 }
+
+  subject(:sp) { described_class.new(s) }
+
+  describe '#distance_to' do
+    subject { sp.distance_to(v) }
+
+    context 'source' do
+      let(:v) { s }
+
+      it { is_expected.to be_zero }
+    end
+
+    context 'when 1 edge is relaxed' do
+      let(:v) { 1 }
+
+      before do
+        sp.relax(Algorithms::DataStructures::Graphs::DirectedEdge.new(s, v, 100))
+      end
+
+      it { is_expected.to eq(100) }
+    end
+
+    context 'when 2 edges are relaxed' do
+      let(:w) { 1 }
+      let(:v) { 2 }
+
+      before do
+        sp.relax(Algorithms::DataStructures::Graphs::DirectedEdge.new(s, w, 100))
+        sp.relax(Algorithms::DataStructures::Graphs::DirectedEdge.new(w, v, 50))
+      end
+
+      it { is_expected.to eq(150) }
+    end
+
+    context 'when there is no path to the specified vertex' do
+      let(:v) { 2 }
+
+      it 'raises and error' do
+        expect{ subject }.to raise_error(described_class::Error)
+      end
+    end
+  end
+
+  describe '#path_to' do
+    subject { sp.path_to(v) }
+
+    context 'source' do
+      let(:v) { s }
+
+      it { is_expected.to eq([s]) }
+    end
+
+    context 'when 1 edge is relaxed' do
+      let(:v) { 1 }
+
+      before do
+        sp.relax(Algorithms::DataStructures::Graphs::DirectedEdge.new(s, v, 100))
+      end
+
+      it { is_expected.to eq([0, 1]) }
+    end
+
+    context 'when 2 edges are relaxed' do
+      let(:w) { 1 }
+      let(:v) { 2 }
+
+      before do
+        sp.relax(Algorithms::DataStructures::Graphs::DirectedEdge.new(s, w, 100))
+        sp.relax(Algorithms::DataStructures::Graphs::DirectedEdge.new(w, v, 50))
+      end
+
+      it { is_expected.to eq([0, 1, 2]) }
+    end
+
+    context 'when there is no path to the specified vertex' do
+      let(:v) { 2 }
+
+      it 'raises and error' do
+        expect{ subject }.to raise_error(described_class::Error)
+      end
+    end
+  end
 end
